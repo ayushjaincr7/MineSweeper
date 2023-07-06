59e973b41cde116718614aa6326ebb3148e69aad
ayushjaincr7 <ja7ayush@gmail.com>
2023-07-06 19:08:32 +0530

    add: center frame change bg back to black

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ root.resizable(False,False)
 
 top_frame = Frame(
     root,
-    bg="red", #change later to black
+    bg="black", #change later to black
     width=settings.WIDTH,
     height=utils.height_prct(25)
 )
@@ -22,13 +22,23 @@ top_frame.place(x=0,y=0)
 
 left_frame = Frame(
     root,
-    bg="blue",
+    bg="black",
     width=utils.width_prct(25),
     height=utils.height_prct(75),
 )
 left_frame.place(x=0,y=180)
 
+center_frame = Frame(
+    root,
+    bg="black",
+    width=utils.width_prct(75),
+    height=utils.height_prct(75)
+)
 
+center_frame.place(
+    x=utils.width_prct(25),
+    y=utils.height_prct(25)
+)
 
 
 # run the windows
