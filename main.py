70912e86895d74334fb660b8406574c3bb4c3874
ayushjaincr7 <ja7ayush@gmail.com>
2023-07-06 16:10:01 +0530

    add: top_frame

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from tkinter import *
+from tkinter import ttk
 
 
 root = Tk()
+# override the settings of the window
+root.config(bg="black")
+root.geometry('1440x720')
+root.title('MineSweeper')
+root.resizable(False,False)
 
-root.geometry('1440X720')
+top_frame = Frame(
+    root,
+    bg="red", #change later to black
+    width=1440,
+    height=180
+)
+top_frame.place(x=0,y=0)
 
+# run the windows
 root.mainloop()
 
